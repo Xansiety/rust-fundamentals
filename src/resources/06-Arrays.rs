3d43581f41e8259d62fbbe3525680a83732fe7ce
xansiety <ferando543@outlook.com>
2023-10-28 17:24:20 -0600

    07-Vectors

--- src/resources/06-Arrays.rs
+++ src/resources/06-Arrays.rs
@@ -1,5 +1,6 @@
 fn main() {
     // Arrays
+    // Arrays are fixed size and can't grow or shrink in size
     let items: [i32; 5] = [1, 2, 3, 4, 5];
     println!("The array is: {:?}", items);
     println!("The first item in the array is: {:?}", items[0]);
