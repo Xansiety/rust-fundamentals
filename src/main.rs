1f28f4d1f3ba6b29f54520a26bece247d73ac4d9
xansiety <ferando543@outlook.com>
2023-10-28 16:39:11 -0600

    01-declare variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    //  declare a variable -> let
+    // Should be on snake_case
+    //! Variables are immutable by default
+    //* if you want to make it mutable, add mut keyword
+    let my_name = "Rust";
+    let mut my_mutable_name = "Rust";
+    // my_name = "Python"; //! Error
+    my_mutable_name = "WASM"; //? No Error
 }
